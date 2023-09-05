b4e83fa78a9e1a784be086cdc788aff043c894db
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2023-09-05 13:52:13 -0700

    Creating an AWS IAM User for Ssss Sssss

---
+++ accounts/infosec/users.tf
@@ -0,0 +1,15 @@
+
+module "user_sssss" {
+  source    = "../../modules/iam-user-group"
+  user_name = "SsssSssss"
+
+  user_groups = [
+    aws_iam_group.all_iam_users.name,
+    
+    aws_iam_group.sandbox_developers.name,
+    
+    aws_iam_group.non_prod_developers.name,
+    
+    aws_iam_group.prod_developers.name,
+    ]
+}
