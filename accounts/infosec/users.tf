c37d2ee98d20902439f04ab5301d7615e273628a
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2023-09-05 13:57:11 -0700

    Creating an AWS IAM User for Asdfads Asdfasdf

---
+++ accounts/infosec/users.tf
@@ -0,0 +1,10 @@
+
+module "user_asdfadsa" {
+  source    = "../../modules/iam-user-group"
+  user_name = "AsdfadsAsdfasdf"
+
+  user_groups = [
+    aws_iam_group.all_iam_users.name,
+    aws_iam_group.sandbox_developers.name,aws_iam_group.non_prod_developers.name,aws_iam_group.prod_developers.name,
+  ]
+}
