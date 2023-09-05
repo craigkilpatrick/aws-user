26a3a4036e4d139c59c338e84299c642a2a8c2bf
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2023-09-05 13:42:36 -0700

    Creating an AWS IAM User for Test Asdf

---
+++ accounts/infosec/users.tf
@@ -0,0 +1,10 @@
+
+module "user_testa" {
+  source    = "../../modules/iam-user-group"
+  user_name = "TestAsdf"
+
+  user_groups = [
+    aws_iam_group.all_iam_users.name,
+    aws_iam_group.sandbox_developers.name,aws_iam_group.prod_developers.name,aws_iam_group.non_prod_developers.name,
+  ]
+}
