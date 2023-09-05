b61b7dd884086106dcdbdf70d91976ed15f21f76
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2023-09-05 14:22:52 -0700

    Creating an AWS IAM User for Qerwer Werwe

---
+++ accounts/infosec/users.tf
@@ -0,0 +1,10 @@
+
+module "user_qerwerw" {
+  source    = "../../modules/iam-user-group"
+  user_name = "QerwerWerwe"
+
+  user_groups = [
+    aws_iam_group.all_iam_users.name,
+    aws_iam_group.sandbox_developers.name,aws_iam_group.non_prod_developers.name,aws_iam_group.prod_developers.name,
+  ]
+}
