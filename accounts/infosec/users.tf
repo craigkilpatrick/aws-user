820c8bfbb9b2221eb65d8b200f1ca607f9929a82
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2023-09-05 14:24:00 -0700

    Creating an AWS IAM User for Xcvx Xcsdfs

---
+++ accounts/infosec/users.tf
@@ -0,0 +1,12 @@
+
+module "user_xcvxx" {
+  source    = "../../modules/iam-user-group"
+  user_name = "XcvxXcsdfs"
+
+  user_groups = [
+    aws_iam_group.all_iam_users.name,
+    aws_iam_group.sandbox_developers.name,
+    aws_iam_group.non_prod_developers.name,
+    aws_iam_group.prod_developers.name,
+  ]
+}
